024840b8504c97d01090e6ce3d2f7ae5f9645267
kozik5051 <csglobal2324@gmail.com>
2025-04-18 18:07:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install -y libreoffice
+
+# Установка FFmpeg
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/
+chmod +x /usr/local/bin/ffmpeg
