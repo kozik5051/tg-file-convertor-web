5e402af3d5461eac59347f61a138b54fbde421b7
kozik5051 <csglobal2324@gmail.com>
2025-04-18 11:09:49 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
