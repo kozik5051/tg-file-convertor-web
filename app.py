316fdae08a0b4304d3c7f8e8018a5736b4ddb983
kozik5051 <csglobal2324@gmail.com>
2025-04-18 11:17:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+# app.py
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='.')
+app = Flask(__name__)
 
 @app.route('/')
-def serve_index():
+def index():
     return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
